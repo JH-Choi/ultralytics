accd159bf45372b2d8d2baf8fe03947d47818c72
JH-Choi <jaehoonc44@gmail.com>
2025-02-27 16:59:42 -0500

    Update configs and scripts

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from ultralytics.models.yolo.model import YOLO
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, default='Okutama.yaml', help='dataset.yaml')
 parser.add_argument('--name', type=str, default='train_D1_Morn_val_D1_Morn', help='experiment name')
+parser.add_argument('--model_name', type=str, default=None, help='model name')
 args = parser.parse_args()
 
 # data = 'Okutama.yaml' # Okutama.yaml | Archangel.yaml
@@ -19,11 +20,16 @@ batch = 16
 img=640
 
 # Load a model
-# model = YOLO('yolov8n.yaml')  # build a new model from scratch
-# model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+if args.model_name == 'yolov8n':
+    model = YOLO('yolov8n.yaml')  # build a new model from scratch
+    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+elif args.model_name == 'yolov8s':
+    model = YOLO('yolov8s.yaml')  # build a new model from scratch
+    model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
+elif args.model_name == 'yolov8m':
+    model = YOLO('yolov8m.yaml')  # build a new model from scratch
+    model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
 
-model = YOLO('yolov8s.yaml')  # build a new model from scratch
-model = YOLO('yolov8s.pt')  # load a pretrained model (recommended for training)
 
 results = model.train(data=data, epochs=epochs, name=name, freeze=freeze, batch=batch, imgsz=img)  
 
--- train_synthetic.sh
+++ train_synthetic.sh
@@ -13,20 +13,39 @@
 # python train.py --data $DATA --name $NAME
 
 
-# DATA=Okutama-S2-D1N-v2-eval-D1D2N.yaml
-# NAME=S3_D1M_v1_eval_D1M
-# python train.py --data $DATA --name $NAME
+# MODEL_NAME=yolov8n # yolov8n / yolov8s / yolov8m 
+
+for MODEL_NAME in yolov8n yolov8s yolov8m
+do
+    DATA=Okutama-S2-v1-eval-D1D2N.yaml
+    NAME=$MODEL_NAME/S2_v1_eval_D1D2N
+    python train.py --data $DATA --name $NAME --model_name $MODEL_NAME
+
+    DATA=Okutama-S2-v2-eval-D1D2N.yaml
+    NAME=$MODEL_NAME/S2_v2_eval_D1D2N
+    python train.py --data $DATA --name $NAME --model_name $MODEL_NAME
+
+    DATA=Okutama-S3-v1-eval-D1D2M.yaml
+    NAME=$MODEL_NAME/S3_v1_eval_D1D2M
+    python train.py --data $DATA --name $NAME --model_name $MODEL_NAME 
+
+    DATA=Okutama-S3-v2-eval-D1D2M.yaml
+    NAME=$MODEL_NAME/S3_v2_eval_D1D2M
+    python train.py --data $DATA --name $NAME --model_name $MODEL_NAME  
+done
 
 
-DATA=Okutama-D-D1Mpart-D1D2M.yaml
+
+
+# DATA=Okutama-D-D1Mpart-D1D2M.yaml
 # DATA=Okutama-D-D2Npart-D1D2N.yaml
-MODEL_NAME=yolov8s # yolov8n / yolov8s / yolov8m 
+# MODEL_NAME=yolov8s # yolov8n / yolov8s / yolov8m 
 # NAME=D_1.1.1 # Train 1.2.2 / Test D1D2Noon
 # NAME=$MODEL_NAME/D_1.1.1 # Train 1.2.2 / Test D1D2Noon
 # NAME=$MODEL_NAME/D_2.2.1 # Train 1.2.2 / Test D1D2Noon
-NAME=$MODEL_NAME/D_2.2.4 # Train 1.2.2 / Test D1D2Noon
+# NAME=$MODEL_NAME/D_2.2.4 # Train 1.2.2 / Test D1D2Noon
 # NAME=yolov8s/D_2.2.2 # Train 1.2.2 / Test D1D2Noon
-python train.py --data $DATA --name $NAME
+# python train.py --data $DATA --name $NAME
 
 
 
